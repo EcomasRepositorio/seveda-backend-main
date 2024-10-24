39a70c07894cd1d44b59096befed3e39664caa8b
zunniga <jmoficial2003@gmail.com>
2024-10-24 12:00:44 -0500

    change

--- src/app.ts
+++ src/app.ts
@@ -4,18 +4,14 @@ import { Request, Response, NextFunction } from "express";
 import cors from "cors";
 // export const app = express();
 
-const writerList = ["https://ecomas.pe", "https://www.ecomas.pe"]
+const writerList = ["https://seveda.edu.pe", "https://www.seveda.edu.pe"]
 
-app.use(cors({ 
-  origin: ["localhost:8000"],
-  methods: ['GET','POST','DELETE','PUT'],
-  allowedHeaders: ['Contend-Type' , 'Authorization'],
- }));
+app.use(cors({ origin: writerList }));
 
 app.get("/", (req: Request, res: Response, next: NextFunction) => {
   prisma;
   res.status(200).json({
-    message: "Wellcome to SEVEDA",
+    message: "Wellcome to - SEVEDA API",
   });
 });
 
